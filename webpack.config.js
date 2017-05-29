52986de8823a6efadfa5a327948089d7c3d80e3d
digi0ps <sriru1998@gmail.com>
2017-05-29 15:36:58 +0530

    Install style-loader, css-loader, file-loader and url-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,35 @@ module.exports = {
 				query: {
 					presets: ['react']
 				}
-			}
+			},
+			{
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.jpg$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.png$/,
+                loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.tff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            },
 		]
 	},
 
